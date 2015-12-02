0011fb4b5b0322bba2ecce12042aeb07cf20bf52
Sarat Chandra Pasumarthy <p.saratchandra4357@gmail.com>
2015-12-02 11:40:01 +0100

    added beta gradle project

---
+++ backend/src/main/java/means/Greeter.java
@@ -0,0 +1,7 @@
+package means;
+
+public class Greeter {
+	public String sayHello() {
+			return "Hello world !";
+		}
+	}
---
+++ backend/src/main/java/means/HelloWorld.java
@@ -0,0 +1,11 @@
+package means;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Greeter test = new Greeter();
+		System.out.println(test.sayHello());
+		database.connect();
+	}
+
+}
