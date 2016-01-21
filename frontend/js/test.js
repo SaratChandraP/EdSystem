3437a9eecd53f26c5a7c203dfe019127cbe12b0a
Sarat Chandra Pasumarthy <p.saratchandra4357@gmail.com>
2016-01-21 13:59:13 +0100

    new
    new

--- frontend/js/test.js
+++ frontend/js/test.js
@@ -1,22 +1,8 @@
-angular.module('todoApp', [])
-  .controller('DemoCtrl', function($scope, $http) {
-      refreshAll();
-
-    //  $scope.addTest = function(item) {
-    //  $http.put('/api/test', item).then(function(response) {
-    //    refreshAll();
-    //  });
-    //}
-    //
-    //  $scope.deleteTest= function(id) {
-    //      $http.delete('/api/test/'+id).then(function(response) {
-    //          refreshAll();
-    //      });
-    //  }
-
+angular.module('questionsApp', [])
+  .controller('questionsCtrl', function($scope, $http) {
 
       function refreshAll() {
-      $http.get('/api/test').then(function(response) {
+      $http.get('/api/question').then(function(response) {
         $scope.items= response.data;
           $scope.answers=response.data.answer;
           //$scope.items[i].id;
