f3b3e76cbfea8d7c5743dd72fb7c9e5544b29f7b
navneetchamala <navneet.7293@gmail.com>
2016-01-21 17:14:34 +0530

    single question
    front end change

--- frontend/js/test.js
+++ frontend/js/test.js
@@ -2,22 +2,31 @@ angular.module('todoApp', [])
   .controller('DemoCtrl', function($scope, $http) {
       refreshAll();
 
-      $scope.addTest = function(item) {
-      $http.put('/api/test', item).then(function(response) {
-        refreshAll();
-      });
-    }
+    //  $scope.addTest = function(item) {
+    //  $http.put('/api/test', item).then(function(response) {
+    //    refreshAll();
+    //  });
+    //}
+    //
+    //  $scope.deleteTest= function(id) {
+    //      $http.delete('/api/test/'+id).then(function(response) {
+    //          refreshAll();
+    //      });
+    //  }
 
-      $scope.deleteTest= function(id) {
-          $http.delete('/api/test/'+id).then(function(response) {
-              refreshAll();
-          });
-      }
 
-    function refreshAll() {
+      function refreshAll() {
       $http.get('/api/test').then(function(response) {
         $scope.items= response.data;
           $scope.answers=response.data.answer;
-      });      
+          //$scope.items[i].id;
+          $scope.j=0;
+          $scope.nextQuestion=myFunction()
+          {
+              $scope.j++;
+          }
+
+      });
     }
+
   });
