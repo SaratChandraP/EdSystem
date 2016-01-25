4cd6cfa1b3b0580aca2d17b4e015055781ff3916
Sarat Chandra Pasumarthy <p.saratchandra4357@gmail.com>
2016-01-25 14:27:57 +0100

    today
    today

---
+++ backend/src/test/java/means/db/test/DBtest.java
@@ -0,0 +1,17 @@
+package means.db.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import means.core.Questions;
+
+public class DBtest {
+
+	@Test
+	public void testDB() {
+		Questions questions = new Questions();
+		fail("Not yet implemented");
+	}
+
+}
