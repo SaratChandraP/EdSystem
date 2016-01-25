e8dc73db8d3bc3f790746080e4901926d962bf24
navneetchamala <navneet.7293@gmail.com>
2016-01-25 19:44:05 +0530

    single question
    single question

--- frontend/js/test.js
+++ frontend/js/test.js
@@ -1,21 +1,37 @@
 angular.module('questionsApp', [])
-  .controller('questionsCtrl', function($scope, $http) {
+    .controller('questionsCtrl', function($scope, $http) {
 
-      refreshAll();
+        refreshAll();
 
-      $scope.questionnum = '1';
+        //$scope.questionnum = '1';
 
-      function refreshAll() {
-      $http.get('/api/questions/1').then(function(response) {
-          $scope.questionSingle= response.data;
-          //$scope.items[i].id;
-          //$scope.j=0;
-          //$scope.nextQuestion=myFunction()
-          //{
-          //    $scope.j++;
-          //}
+        function refreshAll() {
+            $http.get('/api/questions').then(function (response) {
+            });
 
-      });
-    }
+            //$scope.questionSingle=response.data;
 
-  });
+            $scope.i=-1;
+
+            $scope.myFunction=function(){
+                $scope.i++;
+                //$scope.questions=data;
+
+                $scope.questions = [{id:'1', question:'Name?', option1:'1', option2:'2', option3:'3',option4:4},
+                    {id:'2',question:'Age?', option1:'2', option2:'2', option3:'3',option4:4},
+                    {id:'3',question:'City?', option1:'3', option2:'2', option3:'3',option4:4},
+                ];
+
+            }
+                //$scope.items[i].id;
+                //$scope.j=0;
+                //$scope.nextQuestion=myFunction()
+                //{
+                //    $scope.j++;
+                //}
+            //});
+
+
+        }
+
+    });
