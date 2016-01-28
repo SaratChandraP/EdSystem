8239908120af8211a156287bda733b1fcfd479b1
Sarat Chandra Pasumarthy <p.saratchandra4357@gmail.com>
2016-01-28 15:43:32 +0100

    frontend
    frontend

--- frontend/js/test.js
+++ frontend/js/test.js
@@ -7,22 +7,30 @@ angular.module('questionsApp', [])
 
         function refreshAll() {
             $http.get('/api/questions').then(function (response) {
+                $scope.questions=response.data;
+
+                $scope.i=-1;
+
+                $scope.myFunction=function(){
+                    $scope.i++;
+                    $scope.questions=[data];
+                }
+
             });
 
-            //$scope.questionSingle=response.data;
+            //$scope.questions=response.data;
+            //
+            //$scope.i=-1;
+            //
+            //$scope.myFunction=function(){
+            //    $scope.i++;
+            //    $scope.questions=[data];
 
-            $scope.i=-1;
+                //$scope.questions = [{id:'1', question:'Name?', option1:'1', option2:'2', option3:'3',option4:4},
+                //    {id:'2',question:'Age?', option1:'2', option2:'2', option3:'3',option4:4},
+                //    {id:'3',question:'City?', option1:'3', option2:'2', option3:'3',option4:4},
+                //];
 
-            $scope.myFunction=function(){
-                $scope.i++;
-                //$scope.questions=data;
-
-                $scope.questions = [{id:'1', question:'Name?', option1:'1', option2:'2', option3:'3',option4:4},
-                    {id:'2',question:'Age?', option1:'2', option2:'2', option3:'3',option4:4},
-                    {id:'3',question:'City?', option1:'3', option2:'2', option3:'3',option4:4},
-                ];
-
-            }
                 //$scope.items[i].id;
                 //$scope.j=0;
                 //$scope.nextQuestion=myFunction()
